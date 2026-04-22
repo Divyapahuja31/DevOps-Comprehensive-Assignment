431fa5562f9df33e0d2cf253646b87030200a820
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2026-04-22 20:57:01 +0530

    clean up frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,61 @@
-import { useEffect, useState } from "react";
-import { getUsers, createUser } from "./api";
+import { useEffect, useState } from 'react'
+import { getUsers, createUser } from './api'
+import './App.css'
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
 
-  useEffect(() => {
-    loadUsers();
-  }, []);
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleNameInput = (e) => setName(e.target.value)
+  const handleEmailInput = (e) => setEmail(e.target.value)
 
   const loadUsers = async () => {
-    const data = await getUsers();
-    setUsers(data);
-  };
+    const data = await getUsers()
+    setUsers(data)
+  }
 
   const handleAdd = async () => {
-    await createUser({ name: "John", email: "john@test.com" });
-    loadUsers();
-  };
+    await createUser({ name, email })
+    loadUsers()
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      await loadUsers()
+    }
+    fetchData()
+  }, [])
 
   return (
     <div>
-      <button onClick={handleAdd}>Add User</button>
+      <h1>User Management</h1>
 
-      {users.map((u) => (
-        <p key={u._id}>{u.name}</p>
-      ))}
+      <div className='inputs'>
+        <input placeholder="Name" onInput={handleNameInput} type='text' />
+        <input placeholder="Email" onInput={handleEmailInput} type='email' />
+        <button onClick={handleAdd}>Add User</button>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((u) => (
+            <tr key={u._id}>
+              <td>{u.name}</td>
+              <td>{u.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
